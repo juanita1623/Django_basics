4f0c534be04befd3d556dcd7a8757cf694ffab7d
juanita1623 <juanam.ruizc@gmail.com>
2024-11-10 08:59:43 -0500

    .

--- vehiculos_mantenimiento/vehiculos/admin.py
+++ vehiculos_mantenimiento/vehiculos/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Vehiculo, mantenimiento
 
+class MantemientoAdmin(admin.ModelAdmin):
+    list_display = ('vehiculo','fecha','observaciones_breve')
+
+    def observaciones_breve(self,obj):
+        return  obj.oberservaciones[:50]
+    observaciones_breve.short_description = 'Observaciones'
+
+
+
 # Register your models here.
 
 admin.site.register(Vehiculo)
